0e0c40e648805261441d56453fd6408aca3f2a6c
Kayla Webb <kaylarwebb@gmail.com>
2017-10-30 17:18:15 -0400

    Include year of birth statement.

--- w1d1exercise.js
+++ w1d1exercise.js
@@ -1,5 +1,7 @@
 function whoAmI(name, age) {
+    const yearOfBirth = 2017-age;
     console.log(`My name is ${name} and I'm ${age} years old.`);
+    console.log(`I was born in ${yearOfBirth}.`);
 }
 
 whoAmI("Tony", 21);

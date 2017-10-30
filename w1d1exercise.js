9d8472c22a09e152bbbabef196b89ed39cacb7ca
Nicci Williams <nicci@nicciwill.com>
2017-10-30 17:29:29 -0400

    Added yearOfBirth function

--- w1d1exercise.js
+++ w1d1exercise.js
@@ -1,7 +1,13 @@
-function whoAmI(name, age) {
-    const yearOfBirth = 2017-age;
-    console.log(`My name is ${name} and I'm ${age} years old.`);
-    console.log(`I was born in ${yearOfBirth}.`);
+function yearOfBirth(age){
+	const yearOfBirth = 2017-age;
+	return yearOfBirth;
 }
+
+function whoAmI(name, age) {
+    console.log(`My name is ${name} and I'm ${age} years old.`);
+    const yob = yearOfBirth(age); 
+    console.log(`I was born in ${yob}.`);
+}
+
 
 whoAmI("Tony", 21);
